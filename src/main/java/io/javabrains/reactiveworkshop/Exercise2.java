9a0a2cbcfb6acbf6e47e45ab0912f7a82e544847
Shahbaz.Ahmad <shazwashere@gmail.com>
2023-05-27 23:48:07 +0530

    chore: adding code for exercise 2 questions

--- src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
@@ -10,9 +10,11 @@ public class Exercise2 {
 
         // Print all numbers in the ReactiveSources.intNumbersFlux stream
         // TODO: Write code here
+        ReactiveSources.intNumbersFlux().subscribe(e-> System.out.println(e));
 
         // Print all users in the ReactiveSources.userFlux stream
         // TODO: Write code here
+        ReactiveSources.userFlux().subscribe(user -> System.out.println(user));
 
         System.out.println("Press a key to end");
         System.in.read();
